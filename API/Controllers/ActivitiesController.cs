aa9114ff511a7576dd09d09bf043f18fcfc3b552
Eric Ernst <eernst@us.ibm.com>
2020-05-28 15:57:32 -0400

    End of Section 16

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -19,7 +19,6 @@ namespace API.Controllers
     }
 
     [HttpGet("{id}")]
-    [Authorize]
     public async Task<ActionResult<ActivityDto>> Details(Guid id)
     {
       return await Mediator.Send(new Details.Query { Id = id });
